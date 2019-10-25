eeca4d8e5ecb8f31bfda614038c4cac3a5f86aaa
Tomasz Martyński <tomek@martynski.pl>
2019-10-25 13:37:28 +0200

    add 2 new types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,7 @@
+export * from "./ClassMethodNames";
 export * from "./Constructable";
 export * from "./DeepReadonly";
+export * from "./Nullable";
 export * from "./OptionalSpread";
 export * from "./SyncOrAsync";
 export * from "./ValueOf";

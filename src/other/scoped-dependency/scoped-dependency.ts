3d7b1a2d3b63f4545506a3451a4b43a23cdd127f
Tomasz Martyński <tomek@martynski.pl>
2019-12-06 21:35:17 +0100

    asd

--- src/other/scoped-dependency/scoped-dependency.ts
+++ src/other/scoped-dependency/scoped-dependency.ts
@@ -16,11 +16,12 @@ export const scoped = (options: IScopedOptions = {}): any => (
 ) => {
   const Class = interceptable({
     afterConstruct: target => {
-      // console.log(`After constructing class(${target.constructor.name})...`);
       const external = InterceptableContext.getContext(target, InterceptableContextType.External);
 
       if (!external) {
-        throw new Error("pizda nad glowa");
+        throw new Error(
+          `Scoped::afterConstruct couldn't find external instance. Is that even possible?`
+        );
       }
 
       ScopedInternals.createScoped(external);
@@ -34,15 +35,7 @@ export const scoped = (options: IScopedOptions = {}): any => (
         options.onInstanceCreation(external);
       }
     },
-    set: (target, key, value, isInternal) => {
-      target[key] = value;
-      // console.log(
-      //   `Setting class(${target.constructor.name}) key(${String(key)}) to `,
-      //   value,
-      //   `isInternal(${isInternal})...`
-      // );
-      const child = target[key];
-
+    set: (target, key, child, isInternal) => {
       if (!ScopedInternals.isScoped(child)) {
         return true;
       }
@@ -53,7 +46,9 @@ export const scoped = (options: IScopedOptions = {}): any => (
       );
 
       if (!externalParent) {
-        throw new Error("pizda nad glowa");
+        throw new Error(
+          `Scoped::set couldn't find external parent instance. Is that even possible?`
+        );
       }
 
       ScopedInternals.createScoped(externalParent);
@@ -67,65 +62,6 @@ export const scoped = (options: IScopedOptions = {}): any => (
 
   return Class;
 };
-
-// export const scoped = (options: IScopedOptions = {}): any => (
-//   originalConstructor: new (...args: any[]) => any
-// ) => {
-//   const original = originalConstructor;
-
-//   // the new constructor behaviour
-//   const f: any = function(...args: any[]) {
-//     // @ts-ignore
-//     const that = (this as unknown) as any;
-
-//     that.bbb = 123;
-
-//     const result = original.apply(that, args);
-
-//     ScopedInternals.createScoped(that);
-
-//     if (options.name) {
-//       ScopedInternals.applyCustomName(that, options.name);
-//     }
-
-//     if (options.onInstanceCreation) {
-//       options.onInstanceCreation(that);
-//     }
-
-//     return result;
-//   };
-
-//   // copy prototype so intanceof operator still works
-//   f.prototype = original.prototype;
-
-//   // return new constructor (will override original)
-//   return f;
-// };
-
-// export const scoped = (options: IScopedOptions = {}): any => (
-//   originalConstructor: new (...args: any[]) => any
-// ) => {
-//   const Class = class extends originalConstructor {
-//     constructor(...args: any[]) {
-//       super(...args);
-
-//       ScopedInternals.createScoped(this);
-
-//       if (options.name) {
-//         ScopedInternals.applyCustomName(this, options.name);
-//       }
-
-//       if (options.onInstanceCreation) {
-//         options.onInstanceCreation(this);
-//       }
-//     }
-//   };
-
-//   // Fix class name!
-//   Object.defineProperty(Class, "name", { value: originalConstructor.name });
-
-//   return Class;
-// };
 
 export const listScopes = (instance: Object): IScopedContext[] =>
   ScopedInternals.getScopedContexts(instance);

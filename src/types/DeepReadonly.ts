7ccc4d1d9b994b4974154f03cdb0558a2c346957
Tomasz Martyński <tomek@martynski.pl>
2020-01-08 23:09:26 +0100

    fix: DeepReadonly

--- src/types/DeepReadonly.ts
+++ src/types/DeepReadonly.ts
@@ -1,11 +1,12 @@
-export type DeepReadonly<T> = T extends (infer R)[]
-  ? DeepReadonlyArray<R>
-  : T extends Function
-  ? T
-  : T extends object
-  ? DeepReadonlyObject<T>
-  : T;
-
-interface DeepReadonlyArray<T> extends ReadonlyArray<DeepReadonly<T>> {}
+// prettier-ignore
+export type DeepReadonly<T> =
+  T extends Map<infer U, infer V> ? ReadonlyMap<DeepReadonly<U>, DeepReadonly<V>> :
+  T extends Set<infer U> ? ReadonlySet<DeepReadonly<U>> :
+  T extends Promise<infer U> ? Promise<DeepReadonly<U>> :
+  T extends Primitive ? T :
+  T extends Function ? T :
+  T extends Object ? DeepReadonlyObject<T> : never;
 
 type DeepReadonlyObject<T> = { readonly [P in keyof T]: DeepReadonly<T[P]> };
+
+type Primitive = string | number | boolean | undefined | null | symbol | bigint;
